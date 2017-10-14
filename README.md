9b99bbec6dc7d39b09c208b0dbe5e3303bb5665d
Benedict Florance <benedictflorance.a@gmail.com>
2017-10-14 14:31:24 +0530

    Refactore code and added comments except progress bar

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# ucon-2017-workshop
+code for the ucon'17 spider web and app workshop
+
---
+++ app.js
@@ -0,0 +1,57 @@
+
+/*
+Require dependencies
+*/
+
+var express = require('express'); //express is used for a webframework
+var app = require('express')(); //creates a new instance of express 
+var http = require('http').Server(app); //http package is used HTTP server
+var io = require('socket.io')(http); //socket.io package for creating socket connections
+
+app.use(express.static('public')); 
+
+
+
+/*
+Writing a route
+*/
+
+// when client requests a '/' from the browser, we serve 'random_function.html'
+app.get('/', function(req, res){
+  res.sendfile('random_function.html');
+});
+
+
+
+/*
+Writing the socket funtions
+*/
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket){
+  console.log('A user connected');
+
+  //Whenever 'clientEvent' occurs this gets executed
+  socket.on('clientEvent', function(data){
+
+      //The data from clientEvent is sent as 'serverEvent' to whoever listens to 'serverEvent'
+  		socket.broadcast.emit('serverEvent',data);
+  });
+
+  //Whenever someone disconnects this piece of code executed
+  socket.on('disconnect', function () {
+    console.log('A user disconnected');
+  });
+
+});
+
+//Whenever someone attemtps to connect, this gets executed
+io.on('connect',function(socket){
+	console.log("Attempted connect");
+});
+
+
+//Finally, we set the server to listen on port - 3000
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
