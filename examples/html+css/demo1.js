ea348f73b095396244c272e822ab85779057606b
someshrf <someshsundaram@gmail.com>
2017-10-28 14:04:48 +0530

    JS file for button onclick examples

---
+++ examples/html+css/demo1.js
@@ -0,0 +1,18 @@
+function ChangeText(){
+document.getElementById('div1').innerHTML="content changed";
+}
+
+//var ColorCount=1;
+
+function ChangeColor(){
+
+//if(ColorCount==1)
+	document.getElementById('div2').style.color="red";
+
+//else
+  //  document.getElementById('div2').style.color="black";
+
+//ColorCount*=-1;
+
+}
+
